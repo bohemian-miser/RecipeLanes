f9645d7993c020f04abcb041212f87c2328937ef
bohemian-miser <158785140+bohemian-miser@users.noreply.github.com.>
2025-12-20 10:31:19 +0000

    Add mathjax-full to transpilePackages to fix Penrose

--- recipe-rpg-simple/next.config.ts
+++ recipe-rpg-simple/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@penrose/core'],
+  transpilePackages: ['@penrose/core', 'mathjax-full'],
   experimental: {
     serverActions: {
       bodySizeLimit: '5mb',

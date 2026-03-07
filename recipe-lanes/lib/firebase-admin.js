27c50017d363517c198eeb24667d10e8263c543b
bohemian-miser <158785140+bohemian-miser@users.noreply.github.com.>
2026-03-07 05:26:31 +0000

    adding tests

---
+++ recipe-lanes/lib/firebase-admin.js
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2026 Bohemian Miser <https://substack.com/@bohemianmiser>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.auth = exports.storage = exports.db = exports.isFirebaseEnabled = void 0;
+var app_1 = require("firebase-admin/app");
+var firestore_1 = require("firebase-admin/firestore");
+var storage_1 = require("firebase-admin/storage");
+var auth_1 = require("firebase-admin/auth");
+var projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+var storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+var serviceAccountKey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+var credential;
+if (serviceAccountKey) {
+    try {
+        credential = (0, app_1.cert)(JSON.parse(serviceAccountKey));
+    }
+    catch (e) {
+        console.warn('Failed to parse FIREBASE_SERVICE_ACCOUNT_KEY, falling back to default credentials:', e);
+    }
+}
+// 1. Force Admin SDK to talk to the Emulator
+if (process.env.STORAGE_EMULATOR_HOST) {
+    process.env.FIREBASE_STORAGE_EMULATOR_HOST = process.env.STORAGE_EMULATOR_HOST;
+}
+if (process.env.FIRESTORE_EMULATOR_HOST) {
+    process.env.FIREBASE_FIRESTORE_EMULATOR_HOST = process.env.FIRESTORE_EMULATOR_HOST;
+}
+if (process.env.FIREBASE_AUTH_EMULATOR_HOST) {
+    process.env.FIREBASE_AUTH_EMULATOR_HOST = process.env.FIREBASE_AUTH_EMULATOR_HOST;
+}
+// Enable Firebase if explicit keys exist, OR if running in production, OR if Project ID is present (ADC), OR if Emulators are active
+exports.isFirebaseEnabled = !!(serviceAccountKey || process.env.GOOGLE_APPLICATION_CREDENTIALS || process.env.NODE_ENV === 'production' || projectId || process.env.FIREBASE_AUTH_EMULATOR_HOST);
+var app = (0, app_1.getApps)().length > 0 ? (0, app_1.getApp)() : (0, app_1.initializeApp)(__assign({ projectId: projectId, storageBucket: storageBucket }, (credential ? { credential: credential } : {})));
+// 1. Initialize App
+var db = (0, firestore_1.getFirestore)(app);
+exports.db = db;
+var storage = (0, storage_1.getStorage)(app);
+exports.storage = storage;
+var auth = (0, auth_1.getAuth)(app);
+exports.auth = auth;
+// Force the Environment Variable BEFORE initialization (Restored support for NEXT_PUBLIC_USE_FIREBASE_EMULATOR)
+if (process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === 'true') {
+    if (!process.env.FIREBASE_AUTH_EMULATOR_HOST)
+        process.env.FIREBASE_AUTH_EMULATOR_HOST = '127.0.0.1:9099';
+    if (!process.env.FIRESTORE_EMULATOR_HOST)
+        process.env.FIRESTORE_EMULATOR_HOST = '127.0.0.1:8080';
+    if (!process.env.FIREBASE_STORAGE_EMULATOR_HOST)
+        process.env.FIREBASE_STORAGE_EMULATOR_HOST = '127.0.0.1:9199';
+    console.log("🔥 Admin SDK switching to Emulator mode via Env Vars");
+}
